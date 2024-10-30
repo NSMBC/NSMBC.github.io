7a35815318e3b3d4b96ad903b34f9f177f6c742c
Nolan Y? <ndymario@gmail.com>
2024-10-30 15:29:25 -0500

    Update mod list look

--- generate_mods_list.py
+++ generate_mods_list.py
@@ -6,7 +6,7 @@ output_file = "./mods/index.html"
 
 mod_dirs = [d for d in os.listdir(mods_path) if os.path.isdir(os.path.join(mods_path, d)) and d.lower() != "example"]
 
-mod_list = "\n".join([f'\t\t\t<li><a href="{mod}/">{mod}</a></li>' for mod in mod_dirs])
+mod_list = "\n".join([f'\t\t\t<li><a href="{mod}/" class="dialog-link">{mod}</a></li>' for mod in mod_dirs])
 
 with open(template_file, "r") as f:
     template_content = f.read()
